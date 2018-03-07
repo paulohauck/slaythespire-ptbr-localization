028bdef15783bb2a4ae54dd3c8a39614db982f5c
paulovhe <paulovhe@gmail.com>
2018-03-07 09:10:55 -0300

    Update 06-03-2018

--- localization/validate_localization.py
+++ localization/validate_localization.py
@@ -4,11 +4,27 @@ import os
 from functools import partial, reduce
 from itertools import chain
 
+# a list of files to be ignored.
+ignored_files = []
 
 if sys.version_info.major < (3):
     raise Exception("must use python 3")
 else:
     from json.decoder import JSONDecodeError
+
+
+class LintError(Exception):
+    def __str__(self):  # Override to include exception name in exception string
+        return "{}: {}".format(self.__class__.__name__, self.args[0])
+
+class TypeMismatch(LintError):
+    pass
+
+class UnsharedDictKeys(LintError):
+    pass
+
+class ArraySizeMismatch(LintError):
+    pass
 
 
 def jsonify(j):
@@ -23,19 +39,19 @@ def truncate_list(l):
 def compare(a, b, path):
     cmp = partial(compare, path=path)
     if type(a) != type(b):
-        return [(path, "Unmatching types: '{}' and '{}'".format(a, b))]
+        return [(path, TypeMismatch("'{}' and '{}'".format(a, b)))]
     elif isinstance(a, dict):
         a_keys = set(a.keys())
         b_keys = set(b.keys())
         set_diff = a_keys.symmetric_difference(b_keys)
         if set_diff != set():
-            return [(path, "Unshared Dict Keys: '{}'".format(set_diff))]
+            return [(path, UnsharedDictKeys("'{}'".format(set_diff)))]
         else:
             return flatten([cmp(x[0][1], x[1][1]) for x in zip(sorted(a.items()), sorted(b.items()))])
     elif isinstance(a, list):
         if len(a) != len(b):
             lang_pack = os.path.basename(os.path.dirname(path))
-            return [(path, "Different lengthed lists (eng={} vs {}={}): search by '{}' or '{}' for eng or {} respectively".format(len(a), lang_pack, len(b), truncate_list(a), truncate_list(b), lang_pack))]
+            return [(path, ArraySizeMismatch("(eng={} vs {}={}): search by '{}' or '{}' for eng or {} respectively".format(len(a), lang_pack, len(b), truncate_list(a), truncate_list(b), lang_pack)))]
         else:
             return flatten(map(lambda x:cmp(*x), zip(b, a)))
     else:
@@ -82,6 +98,12 @@ if __name__ == "__main__":
     other_jsons = [get_json_files(x) for x in lang_packs]
     compare_against_eng = partial(compare_lang_pack, eng_json)
     errors = flatten(map(compare_against_eng, other_jsons))
+
+    # Filter out errors in file ignore list. The more complicated rules are hardcoded to save time.
+    errors = [e for e in errors if not (os.path.basename(e[0]) in ignored_files)]
+    errors = [e for e in errors if not (os.path.basename(e[0]) in "keywords.json" and isinstance(e[1], ArraySizeMismatch))]
+    errors = [e for e in errors if not (os.path.basename(e[0]) == "cards.json" and isinstance(e[1], UnsharedDictKeys) and 'UPGRADE_DESCRIPTION' in str(e[1]))]
+
     if len(errors) == 0:
         print("SUCCESS. No errors found.")
         sys.exit(0)
@@ -89,8 +111,11 @@ if __name__ == "__main__":
         error_count = len(errors)
         error_limit = 5
         if error_count > error_limit:
-            trunc = "(showing the first `{}`)".format(error_limit)
-        print("WARNING! `{}` error{} detected in localization json files {}:".format(error_count, 's' if error_count > 1 else '', trunc))
+            trunc_msg = "(showing the first `{}`)".format(error_limit)
+        else:
+            trunc_msg = ""
+        plurality = 's' if error_count > 1 else ''
+        print("WARNING! `{}` error{} detected in localization json files {}:".format(error_count, plurality, trunc_msg))
         def error_format(errors, limit):
             return list(map(lambda e:"In file: {}\n{}".format(e[0],e[1]), errors[:limit]))
         msg = "```{}```".format("\n\n".join(error_format(errors, limit=error_limit)))
